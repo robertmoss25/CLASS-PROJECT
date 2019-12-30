52950b3e35cd4f67bead29f78cbc5af3664f4493
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2019-12-30 12:10:11 -0500

    Using Subject rather than EventEmitter

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private shopListSubscription: Subscription;
 
   constructor(private shoplistService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoplistService.getIngredients();
-    this.shoplistService.ingredientsChanged.subscribe(
+    this.shopListSubscription = this.shoplistService.ingredientsChanged.subscribe(
       (pIngredients: Ingredient[]) => {
         this.ingredients = pIngredients;
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.shopListSubscription.unsubscribe();
   }
 }
