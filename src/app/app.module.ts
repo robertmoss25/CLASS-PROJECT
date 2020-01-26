332f80a670db82ce340333fb55d5b55e8f0f996a
robertmoss25 <42865029+robertmoss25@users.noreply.github.com>
2020-01-26 10:39:09 -0500

    Setting up the data storage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -32,7 +33,7 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeEditComponent
   ],
   imports: [
-    BrowserModule, FormsModule, AppRoutingModule,ReactiveFormsModule
+    BrowserModule, FormsModule, AppRoutingModule,ReactiveFormsModule, HttpClientModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
